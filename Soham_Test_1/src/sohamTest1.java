e63a74c1853d95127b5dafcc8101f0a98fc291eb
Anand Jain <jain.12@gmail.com>
2018-11-02 17:50:20 -0400

    test
    test

--- Soham_Test_1/src/sohamTest1.java
+++ Soham_Test_1/src/sohamTest1.java
@@ -3,7 +3,7 @@ public class sohamTest1 {
 
 	public static void main(String[] args) {
 		String x = "123445";
-		System.out.println(x.indexOf("45"));
+		System.out.println(x.indexOf("12345"));
 	}
 
 }
